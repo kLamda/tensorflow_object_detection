f8d3e1db5761c6eacfc75f09c536dc3cdc949618
kLamda <krishnakumar76068@gmail.com>
2021-09-28 20:59:45 +0530

    Updates

---
+++ precache-manifest.0c71c3612aa746919568f5ddc9a1670b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e02ca83a45bbc44e16d8689023f990",
+    "url": "/tensorflow_object_detection/index.html"
+  },
+  {
+    "revision": "288d76870d74f8eed839",
+    "url": "/tensorflow_object_detection/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2f3d8c7581827937e2c7",
+    "url": "/tensorflow_object_detection/static/js/2.1b9fb303.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/tensorflow_object_detection/static/js/2.1b9fb303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288d76870d74f8eed839",
+    "url": "/tensorflow_object_detection/static/js/main.0ad41e4a.chunk.js"
+  },
+  {
+    "revision": "74abcb8b61e094633f81",
+    "url": "/tensorflow_object_detection/static/js/runtime-main.cdcbaba3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tensorflow_object_detection/precache-manifest.0c71c3612aa746919568f5ddc9a1670b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tensorflow_object_detection/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
