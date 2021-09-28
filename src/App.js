56fa8fa3a08ea373f86c08ddb296bd6da422b4f5
kLamda <krishnakumar76068@gmail.com>
2021-09-28 21:17:52 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef,  useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
@@ -9,32 +9,41 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  // Main function
   const runCoco = async () => {
     const net = await cocossd.load();
     
+    //  Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
+    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
+      // Get Video Properties
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
+      // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      canvasRef.current.width = videoWidth/3;
-      canvasRef.current.height = videoHeight/3;
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
 
       const obj = await net.detect(video);
+
+      // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
+
       drawRect(obj, ctx); 
     }
   };
@@ -55,8 +64,8 @@ function App() {
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: "90%",
-            height: "90%",
+            width: "100%",
+            height: "75%",
           }}
           videoConstraints={{facingMode: "environment"}}
         />
@@ -71,7 +80,8 @@ function App() {
             right: 0,
             textAlign: "center",
             zindex: 8,
-            backgroundColor: "transparent",
+            width: 640,
+            height: 480,
           }}
         />
       </header>
